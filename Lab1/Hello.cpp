4dc8a560a9f3cb7f249de59464521cdb92124341
Naomi-ANN <nevellalex@gmail.com>
2025-09-04 15:08:45 -0700

    Classwork

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+//ECE 2310 Hello world redo
+/*
+I am using two diffrent ways to output the words in quotations
+*/
+    int main(){
+        cout <<"Hello world!!!"<< endl;
+            printf("Hello once again world!!!\n");
+                return 0;
+    }
